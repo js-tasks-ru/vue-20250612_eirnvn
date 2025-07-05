2fdaecefce2cc5c5306f201e8830e1fd51368575
Ирина <eirnvn@mail.ru>
2025-07-05 13:06:17 +0300

    Broken-map WatchEffect+pinRef Tests passed

--- 02-basics-2/20-broken-map/MapApp.js
+++ 02-basics-2/20-broken-map/MapApp.js
@@ -1,4 +1,4 @@
-import { defineComponent, ref, watch } from 'vue'
+import { defineComponent, ref, watchEffect } from 'vue'
 
 export default defineComponent({
   name: 'MapApp',
@@ -9,6 +9,7 @@ export default defineComponent({
     const x = ref(0)
     const y = ref(0)
 
+    const pinRef = ref(null)
     /**
      * Обработчик клика по карте для установки координат метки
      * @param {MouseEvent} event
@@ -19,20 +20,30 @@ export default defineComponent({
     }
 
     // Следим за X и Y для установки нового положения
-    watch(
-      [x, y],
-      (newCoords, oldCoords) => {
-        // Находим метку и изменяем её положение
-        const map = document.querySelector('.pin')
-        map.style.left = `${newCoords[0]}px`
-        map.style.top = `${newCoords[1]}px`
-      },
-      { deep: true, imediate: true },
-    )
+    // watch(
+    //   [x, y],
+    //   (newCoords, oldCoords) => {
+    //     // Находим метку и изменяем её положение
+    //     const map = document.querySelector('.pin')
+    //     map.style.left = `${newCoords[0]}px`
+    //     map.style.top = `${newCoords[1]}px`
+    //   },
+    //   { deep: true, imediate: true },
+    // )
+
+    watchEffect(() => {
+      // Находим метку и изменяем её положение
+      // const map = document.querySelector('.pin')
+      if (pinRef.value) {
+        pinRef.value.style.left = `${x.value}px`
+        pinRef.value.style.top = `${y.value}px`
+      }
+    })
 
     return {
       x,
       y,
+      pinRef,
       handleClick,
     }
   },
@@ -40,7 +51,7 @@ export default defineComponent({
   template: `
       <div class="map" @click="handleClick">
         <img class="map-image" src="./map.png" alt="Map" draggable="false" />
-        <span class="pin">📍</span>
+        <span ref="pinRef" class="pin">📍</span>
       </div>
     `,
 })
